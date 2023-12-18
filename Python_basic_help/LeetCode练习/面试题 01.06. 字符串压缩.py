2a941fb5bbca3c3c9e909a6b6d0752fc3898febb
bote798 <3551575195@qq.com>
2023-12-18 10:30:58 +0800

    添加文件

---
+++ Python_basic_help/LeetCode练习/面试题 01.06. 字符串压缩.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+# @Project : 力扣 
+# @File    : 面试题 01.06. 字符串压缩.py
+# @Author  : bote798
+# @Date    : 2023/12/18 9:12 
+# @IDE     : PyCharm
+
+str_1 = "aabcccccaaa"
+
+# 长度
+lenth_1 = len(str_1)
+"""
+不推荐使用字符串直接 res += s[i] 拼接，
+在 Python 中字符串是「不可变对象」，每次字符串拼接都会生成一个新字符串，效率低下。
+推荐使用列表，先将结果按照顺序添加，最终返回前拼接为字符串，因此只需要一次拼接操作。
+                            作者：Krahets
+                            来源：力扣（LeetCode）
+"""
+list_1 = list()
+i = 0
+j = 0
+# 计数器
+count = 0
+# i,j均不可超出字符串的长度
+while i < lenth_1:
+    while j < lenth_1 and str_1[i] == str_1[j]:
+        # 如果i,j所指的字母相同,则计数器加1
+        count = count + 1
+        # j+1指向下一个字节
+        j = j + 1
+    # 若当前所指的两个字母不相同,则将之前i所指的字母与个数依次存入列表
+    list_1.append(str_1[i])
+    list_1.append(str(count))
+    # 对计数器归零
+    count = 0
+    # 将j当前值赋给i,时i指向下一个不同的字母
+    i = j
+# 直接使用join将列表转为字符串
+print(''.join(list_1))
+
+# 正式解题
+# i, j, lenth = 0, 0, len(S)
+# list_1 = list()
+# while i < lenth:
+#     while j < lenth and S[i] == S[j]:
+#         j = j + 1
+#     list_1.append(S[i])
+#     # j与i的差值就是字母数量（这样省去了计数器）
+#     list_1.append(str(j - i))
+#     i = j
+# new_str = ''.join(list_1)
+# 题目的最后一个要求,压缩后的长度小于原字符串,则输出,否则输出原字符串
+# return new_str if len(new_str) < lenth else S
